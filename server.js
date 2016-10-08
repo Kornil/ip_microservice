0c03af03088f977a01631c5841302a6f007bc97c
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2016-10-08 21:25:25 +0000

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
+var result = {};
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    result.ipaddress = req.headers['x-forwarded-for'];
+    var lang = req.headers['accept-language'].split(",");
+    result.language = lang[0];
+    var start = req.headers['user-agent'].indexOf("("),
+    end = req.headers['user-agent'].indexOf(")");
+    result.software = req.headers['user-agent'].substring(start+1,end);
+    res.send(result);
 }).listen(8080);
